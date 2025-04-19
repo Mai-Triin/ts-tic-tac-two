5bfc7040542a82e8c052ee3dc14149511ab3f325
mai-triinkaseoja <mai.triin.kaseoja@gmail.com>
2025-04-19 19:50:23 +0300

    second commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+// import { resolve } from 'path';
+// const { defineConfig } = require('vite');
+
+export default defineConfig({
+  base: "/ts-tic-tac-two",
+});
+
+
+// const defineConfig = ({
+//   base: "/ts-tic-tac-two",
+// });
+// export default defineConfig;
